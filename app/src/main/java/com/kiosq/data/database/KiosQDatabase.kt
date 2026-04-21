04990b2b7b30e9014844ebe119a1cded9fd12801
Trisna-Nadi-Selamet <nadi.selamet@gmail.com>
2026-04-22 04:36:19 +0700

    FIX DONE SEMUA - PASTI BUILD

--- app/src/main/java/com/kiosq/data/database/KiosQDatabase.kt
+++ app/src/main/java/com/kiosq/data/database/KiosQDatabase.kt
@@ -1,16 +1,12 @@
 package com.kiosq.data.database
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
+import androidx.room.*
 import com.kiosq.data.dao.BarangDao
 import com.kiosq.data.dao.TransaksiDao
-import com.kiosq.data.entity.Barang
-import com.kiosq.data.entity.Transaksi
-import androidx.room.TypeConverters
-
-
+import com.kiosq.data.entity.*
+import com.kiosq.data.entity.SatuanConverter
+import com.kiosq.data.entity.TransaksiConverter
 
 @Database(
     entities = [Barang::class, Transaksi::class],
@@ -24,7 +20,9 @@ abstract class KiosQDatabase : RoomDatabase() {
     abstract fun transaksiDao(): TransaksiDao
 
     companion object {
-        @Volatile private var INSTANCE: KiosQDatabase? = null
+
+        @Volatile
+        private var INSTANCE: KiosQDatabase? = null
 
         fun getInstance(context: Context): KiosQDatabase {
             return INSTANCE ?: synchronized(this) {
@@ -32,8 +30,9 @@ abstract class KiosQDatabase : RoomDatabase() {
                     context.applicationContext,
                     KiosQDatabase::class.java,
                     "kiosq_database"
-                ).fallbackToDestructiveMigration()
-                 .build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
 
                 INSTANCE = instance
                 instance
