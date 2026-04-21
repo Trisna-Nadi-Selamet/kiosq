ef28add54fdffcabc3a85285288a4813b11f052b
Trisna-Nadi-Selamet <nadi.selamet@gmail.com>
2026-04-22 00:45:41 +0700

    build new defedency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,12 +23,26 @@ android {
 
     buildFeatures {
         compose = true
+        viewBinding = true
     }
 
-    dependencies {
+    // 🔥 FIX JVM ERROR
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+// 🔥 HARUS DI LUAR android{}
+dependencies {
+
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
-    }
+
 }
